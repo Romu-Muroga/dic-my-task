789e84009fe9f27fbf9d41e4ac2392e04ae06cd4
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2018-12-17 18:12:43 +0900

    編集、削除機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_params, only: [:edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -17,12 +19,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-    if @task.save
+    if @task.update(task_params)
       redirect_to tasks_path, notice: "編集しました。"
     else
       render "edit"
@@ -30,12 +30,15 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, notice: "削除しました。"
   end
 
   private
+
+  def set_params
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :content)
