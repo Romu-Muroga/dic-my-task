a7e0fe4a45b613fce9dfe25aa47bd81322929055
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2018-12-17 17:18:51 +0900

    作成画面実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,21 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "登録しました。"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
