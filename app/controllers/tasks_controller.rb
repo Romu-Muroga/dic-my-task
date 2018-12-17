d3a879f403552fbee01234c8bf7efe7916213f67
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2018-12-17 17:50:54 +0900

    編集画面作成途中、デバックツールインストール

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,25 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.save
+      redirect_to tasks_path, notice: "編集しました。"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "削除しました。"
+  end
+
   private
 
   def task_params
