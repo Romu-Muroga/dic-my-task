9b2e529c9bb20cfa12b1d557c918551a2fa3754b
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2018-12-18 10:39:41 +0900

    マージするためのコミット

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :set_params, only: [:show,:edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "登録しました。"
+    else
+
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集しました。"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "削除しました。"
+  end
+
+  private
+
+  def set_params
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end
