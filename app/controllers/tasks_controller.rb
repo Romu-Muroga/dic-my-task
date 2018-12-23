14c4e1efbcfee97809919f38d390a6046fa3418a
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2018-12-23 16:19:40 +0900

    taskモデルにバリデーション・scopeを実装、DBに制約を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_params, only: [:show,:edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.order("created_at DESC")
+    @tasks = Task.all.sorted
   end
 
   def new
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,17 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Task, type: :model do
 
   it "titleが空ならバリデーションが通らない" do
-    task = Task.new(title: '', content: '失敗テスト')
-    expect(task).not_to be_valid
+    task = Task.new(title: "", content: "失敗テスト")
+    expect(task).not_to be_valid#be_validでvalid?メソッドを呼んで、それが返す値が（真 = true）ではない（not_to）ことを期待するテスト
   end
 
   it "contentが空ならバリデーションが通らない" do
-    # ここに内容を記載する
+    task = Task.new(title: "失敗テスト", content: "")
+    expect(task).not_to be_valid
   end
 
   it "titleとcontentに内容が記載されていればバリデーションが通る" do
-    # ここに内容を記載する
+    task = Task.new(title: "成功テスト", content: "成功テスト")
+    expect(task).to be_valid#be_validでvalid?メソッドを呼んで、それが返す値が（真 = true）である（to）ことを期待するテスト
   end
 end
