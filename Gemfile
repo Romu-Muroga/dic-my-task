38549b318f069ecd67a46339546da65b8a1a5dea
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-01-08 20:35:55 +0900

    ページネーションを追加

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # enumをi18n化するgem
 gem 'enum_help'
 
+# ページネーション機能(railsが５系なのでバージョン指定)
+gem 'kaminari', '~> 0.17.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,14 @@
 class TasksController < ApplicationController
   before_action :set_params, only: [:show, :edit, :update, :destroy]
+  PER = 8
 
   def index
     if params[:etl_sort]
-      @tasks = Task.end_time_limit_sorted.limit(30)#all省略
+      @tasks = Task.page(params[:page]).per(PER).end_time_limit_sorted#all省略
     elsif params[:pri_sort]
-      @tasks = Task.priority_sorted.limit(30)#all省略
+      @tasks = Task.page(params[:page]).per(PER).priority_sorted#all省略
     else
-      @tasks = Task.created_at_sorted.limit(30)#all省略
+      @tasks = Task.page(params[:page]).per(PER).created_at_sorted#all省略
     end
   end
 
@@ -16,36 +17,36 @@ class TasksController < ApplicationController
   #   # 「if文中のnilはfalseとして扱われる」if文中で、falseと同等の値として扱われるのはnilだけ。
   #   # falseとnil以外の値、たとえば、0、[],””などはtrueとして扱われる。
   #   if params[:etl_sort]#返り値がnil
-  #     @tasks = Task.end_time_limit_sorted.limit(30)#all省略
+  #     @tasks = Task.page(params[:page]).per(PER).end_time_limit_sorted#all省略
   #   elsif params[:pri_sort]#返り値がnil
-  #     @tasks = Task.priority_sorted.limit(30)#all省略
+  #     @tasks = Task.page(params[:page]).per(PER).priority_sorted#all省略
   #   # elsif params[:task][:search]#undefined method `[]' for nil:NilClassとなる([]を呼び出そうとしているが、その元のオブジェクトがnilであると言っている。)
   #   elsif params[:task] && params[:task][:search]#params[:task]の返り値がnil
   #     # present?を書かないとフィールドが空欄（""）でも検索しにいってしまいエラーが起こる？
   #     if params[:task][:title].present? && params[:task][:status].present?
-  #       @tasks = Task.title_status_search(params[:task][:title], params[:task][:status])
+  #       @tasks = Task.title_status_search(params[:task][:title], params[:task][:status]).page(params[:page]).per(PER)
   #     elsif params[:task][:title].present?
-  #       @tasks = Task.title_search(params[:task][:title])
+  #       @tasks = Task.title_search(params[:task][:title]).page(params[:page]).per(PER)
   #     elsif params[:task][:status].present?
-  #       @tasks = Task.status_search(params[:task][:status])
+  #       @tasks = Task.status_search(params[:task][:status]).page(params[:page]).per(PER)
   #     elsif params[:task][:title].blank? && params[:task][:status].blank?
   #       redirect_to tasks_path, notice: t("flash.blank")
   #     end
   #   else
-  #     @tasks = Task.created_at_sorted.limit(30)#all省略
+  #     @tasks = Task.page(params[:page]).per(PER).created_at_sorted#all省略
   #   end
   # end
 
   def search
     # present?を書かないとフィールドが空欄（""）でも検索しにいってしまいエラーが起こる？
     if params[:task][:title].present? && params[:task][:status].present?
-      @tasks = Task.title_status_search(params[:task][:title], params[:task][:status])
+      @tasks = Task.title_status_search(params[:task][:title], params[:task][:status]).page(params[:page]).per(PER)
       render "index"
     elsif params[:task][:title].present?
-      @tasks = Task.title_search(params[:task][:title])
+      @tasks = Task.title_search(params[:task][:title]).page(params[:page]).per(PER)
       render "index"
     elsif params[:task][:status].present?
-      @tasks = Task.status_search(params[:task][:status])
+      @tasks = Task.status_search(params[:task][:status]).page(params[:page]).per(PER)
       render "index"
     elsif params[:task][:title].blank? && params[:task][:status].blank?
       redirect_to tasks_path, notice: t("flash.blank")
