bbc2f5d2a2cda64f7e4017149f9d7c9cfb1082f9
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-01-25 11:01:49 +0900

    seeds.rb編集中

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# 1000.times do |n|
-#   10.times do |n|
-#     title = Faker::Pokemon.name
-#     content = Faker::Pokemon.name
-#     end_time_limit = Faker::Date.between(2.days.ago, Date.today)
-#     status = Faker::Number.between(0, 2)
-#
-#     Task.create!(title: title,
-#                  content: content,
-#                  end_time_limit: end_time_limit,
-#                  status: status
-#                  )
-#   end
-# end
-#
+10.times do |n|
+  10.times do |n|
+    title = Faker::Pokemon.name
+    content = Faker::Pokemon.name
+    end_time_limit = Faker::Date.between(2.days.ago, Date.today)
+    status = Faker::Number.between(0, 2)
+
+    Task.create!(title: title,
+                 content: content,
+                 end_time_limit: end_time_limit,
+                 status: status
+                 )
+  end
+end
+
 User.create!(name: "seedデータ",
              email: "seed@dic.com",
              password: "12345678",
