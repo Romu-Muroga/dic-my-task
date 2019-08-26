c4dd5db3a0c83113b290880ed9f23515ee301f31
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-08-26 19:54:10 +0900

    Add strong parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)#downcaseは値は更新しない。
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: session_params[:email].downcase)#downcaseは値は更新しない。
+    if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id
       flash[:success] = t("flash.login_success")
       redirect_to root_path
@@ -18,5 +18,11 @@ class SessionsController < ApplicationController
     session.delete(:user_id)
     flash[:success] = t("flash.logout")
     redirect_to login_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
